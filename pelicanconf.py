1e8f6aa7ffefa0118e80945feea9e19ca80407a2
skarfie123 <rahulpai@rocketmail.com>
2023-03-22 22:52:53 +0000

    pelican-quickstart

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+AUTHOR = 'Rahul Pai'
+SITENAME = 'Rahul Pai'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('GitHub', 'https://github.com/skarfie123'),)
+
+# Social widget
+SOCIAL = (('LinkedIn', 'https://in.linkedin.com/in/r-pai'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
